f93554e8d51f93b9e685123d4ffaa607539ce385
xincao9 <xincao9@gmail.com>
2024-12-15 03:03:46 +0800

    开始搞页面

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,47 +1,105 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <el-container>
+    <el-header>
+      <div class="header-content">
+        <el-logo class="logo" />
+        <div class="search-box">
+          <el-input v-model="searchQuery" placeholder="请输入关键词" class="input-search" />
+          <el-button type="primary" @click="handleSearch">搜索</el-button>
+        </div>
+      </div>
+    </el-header>
+    <el-main>
+      <!-- 搜索结果展示区域 -->
+      <div v-if="searchResults.length > 0" class="search-results">
+        <h2>搜索结果</h2>
+        <ul>
+          <li v-for="result in searchResults" :key="result.id">{{ result.title }}</li>
+        </ul>
+      </div>
+      <div v-else class="no-results">
+        <h2>没有找到相关结果</h2>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: []
+    };
+  },
+  methods: {
+    handleSearch() {
+      // 模拟搜索逻辑，这里可以使用真实的搜索 API
+      if (this.searchQuery) {
+        // 模拟搜索结果
+        this.searchResults = [
+          { id: 1, title: '搜索结果 1：' + this.searchQuery },
+          { id: 2, title: '搜索结果 2：' + this.searchQuery },
+          { id: 3, title: '搜索结果 3：' + this.searchQuery }
+        ];
+      } else {
+        this.searchResults = [];
+      }
+    }
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.header-content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  height: 60px;
+  background-color: #fff;
+  border-bottom: 1px solid #dcdcdc;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  width: 120px;
+  height: 40px;
+  background: url('https://www.baidu.com/img/baidu_jgylogo3.gif') no-repeat center center;
+  background-size: contain;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.search-box {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.input-search {
+  width: 70%;
+  height: 36px;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px 0 0 4px;
+  padding: 0 10px;
+  box-sizing: border-box;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.el-button {
+  height: 36px;
+  border-radius: 0 4px 4px 0;
+  margin-left: -1px; /* To remove the border gap between input and button */
+}
+
+.search-results, .no-results {
+  padding: 20px;
+}
+
+.search-results ul {
+  list-style: none;
+  padding: 0;
+}
+
+.search-results li {
+  margin: 10px 0;
 }
 </style>
