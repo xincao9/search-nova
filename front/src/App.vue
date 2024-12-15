fade72be39d5e3dadca0dbebe741678a141d3736
xincao9 <xincao9@gmail.com>
2024-12-15 18:30:39 +0800

    OK

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,85 +1,34 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {Search} from '@element-plus/icons-vue'
+import logo from '@/assets/img/logo_big.png'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <el-container>
+    <el-header>
+    </el-header>
+    <el-main>
+      <el-row justify="center">
+        <el-col :span="4">
+          <el-image :src="logo"/>
+        </el-col>
+      </el-row>
+      <el-row justify="center">
+        <div style="margin-top: 80px">
+          <el-input style="width: 480px; height: 40px;"/>
+        </div>
+      </el-row>
+      <el-row justify="center">
+        <div style="margin-top: 30px">
+          <el-button plain size="large" :icon="Search">Good Search</el-button>
+          <el-button plain size="large">I'm Feeling Lucky</el-button>
+        </div>
+      </el-row>
+    </el-main>
+    <el-footer>
+    </el-footer>
+  </el-container>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
