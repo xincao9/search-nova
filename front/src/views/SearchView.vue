1df79289e27b1ffbaa4bfff3a62643ef6c2aa91d
xincao9 <xincao9@gmail.com>
2024-12-16 12:02:06 +0800

    OK

--- front/src/views/SearchView.vue
+++ front/src/views/SearchView.vue
@@ -1,24 +1,40 @@
 <script>
 import Logo from "@/assets/logo.svg";
 import {useRoute} from 'vue-router';
+import axios from 'axios';
+import {ref, onMounted} from "vue";
+
 
 export default {
   name: "SearchView",
   setup() {
     const route = useRoute();
     const answer = route.query.answer;
-    const pages = [
-      {
-        "title": "澳门“一国两制”成功实践获得4点重要经验",
-        "describe": "在热烈的掌声中，习近平发表了重要讲话。习近平指出，澳门地方虽小，但在“一国两制”实践中作用独特。总结澳门“一国两制”成功实践，可以获得以下4点重要经验",
-        "url": "https://element-plus.org",
-      },
-      {
-        "title": "澳门“一国两制”成功实践获得4点重要经验",
-        "describe": "在热烈的掌声中，习近平发表了重要讲话。习近平指出，澳门地方虽小，但在“一国两制”实践中作用独特。总结澳门“一国两制”成功实践，可以获得以下4点重要经验",
-        "url": "https://element-plus.org",
+    // const pages = [
+    //   {
+    //     "title": "澳门“一国两制”成功实践获得4点重要经验",
+    //     "describe": "在热烈的掌声中，习近平发表了重要讲话。习近平指出，澳门地方虽小，但在“一国两制”实践中作用独特。总结澳门“一国两制”成功实践，可以获得以下4点重要经验",
+    //     "url": "https://element-plus.org",
+    //   },
+    //   {
+    //     "title": "澳门“一国两制”成功实践获得4点重要经验",
+    //     "describe": "在热烈的掌声中，习近平发表了重要讲话。习近平指出，澳门地方虽小，但在“一国两制”实践中作用独特。总结澳门“一国两制”成功实践，可以获得以下4点重要经验",
+    //     "url": "https://element-plus.org",
+    //   }];
+    const pages = ref(null);
+    const fetchData = async () => {
+      try {
+        const url = "http://localhost:8080/page?text=" + answer;
+        const response = await axios.get(url);
+        const data = response.data;
+        pages.value = data.data;
+      } catch (err) {
+        console.log(err)
+      } finally {
       }
-    ]
+    };
+
+    onMounted(fetchData);
     return {Logo, answer, pages}
   }
 }
