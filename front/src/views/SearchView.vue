2ff12d0d538612781f34ab01965bc5799c7d76d2
xincao9 <xincao9@gmail.com>
2024-12-16 10:38:54 +0800

    OK

--- front/src/views/SearchView.vue
+++ front/src/views/SearchView.vue
@@ -19,8 +19,17 @@ export default {
         <el-image :src="Logo" style="width: 40px; height: 40px;"/>
         <el-input style="width: 480px; height: 40px; margin-left: 10px" v-model="answer"/>
       </el-row>
+      <el-row>
+        <el-divider/>
+      </el-row>
     </el-header>
-    <el-main>
+    <el-main style="margin-top:50px; margin-left: 20px">
+      <el-row style="margin-top: 15px">
+        <el-text size="large">Large</el-text>
+      </el-row>
+      <el-row style="margin-top:20px">
+        <el-text size="default">Large</el-text>
+      </el-row>
     </el-main>
     <el-footer>
     </el-footer>
