d6dc55ac1ce7ea925876d4639814925fb7410166
xincao9 <xincao9@gmail.com>
2024-12-16 16:30:21 +0800

    OK

--- front/src/views/SearchView.vue
+++ front/src/views/SearchView.vue
@@ -10,18 +10,7 @@ export default {
     const router = useRouter();
     const initialAnswer = route.query.answer || '';
     const answer = ref(initialAnswer);
-    const initialPages = [
-      {
-        "title": "凤凰,凤凰网,凤凰新媒体,凤凰卫视,凤凰卫视中文台,资讯台,电影台,欧洲台,美洲台,凤凰周刊",
-        "describe": "凤凰网是中国领先的综合门户网站，提供含文图音视频的全方位综合新闻资讯、深度访谈、观点评论、财经产品、互动应用、分享社区等服务，同时与凤凰无线、凤凰宽频形成三屏联动，为全球主流华人提供互联网、无线通信、电视网三网融合无缝衔接的新媒体优质体验。",
-        "url": "https://www.ifeng.com"
-      },
-      {
-        "title": "生活故事,架构,大数据,一线,FastDFS,开发者,编程,代码,开源,IT网站",
-        "describe": "本站是纯洁的微笑的技术分享博客。内容涵盖生活故事、Java后端技术、Spring Boot、Spring Cloud、微服务架构、大数据演进、高可用架构、中间件使用、系统监控等相关的研究与知识分享。",
-        "url": "https://www.ityouknow.com"
-      }
-    ];
+    const initialPages = [];
     const pages = ref(initialPages);
 
     const fetchData = async () => {
@@ -35,7 +24,8 @@ export default {
     };
 
     const handleSearch = () => {
-      fetchData()
+      router.replace({query: {answer: answer.value}});
+      fetchData();
     };
 
     onMounted(fetchData);
@@ -71,6 +61,7 @@ export default {
         <el-row>
           <text style="margin-top:10px; font-size: 16px">{{ page.describe }}</text>
         </el-row>
+        <el-divider/>
       </div>
     </el-main>
     <el-footer>
