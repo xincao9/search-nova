1b58f132b2030731845a53bf6fe521c75b9a4f46
xincao9 <xincao9@gmail.com>
2024-12-16 11:06:33 +0800

    OK

--- front/src/views/SearchView.vue
+++ front/src/views/SearchView.vue
@@ -7,7 +7,19 @@ export default {
   setup() {
     const route = useRoute();
     const answer = route.query.answer;
-    return {Logo, answer}
+    const pages = [
+      {
+        "title": "澳门“一国两制”成功实践获得4点重要经验",
+        "describe": "在热烈的掌声中，习近平发表了重要讲话。习近平指出，澳门地方虽小，但在“一国两制”实践中作用独特。总结澳门“一国两制”成功实践，可以获得以下4点重要经验",
+        "url": "https://element-plus.org",
+      },
+      {
+        "title": "澳门“一国两制”成功实践获得4点重要经验",
+        "describe": "在热烈的掌声中，习近平发表了重要讲话。习近平指出，澳门地方虽小，但在“一国两制”实践中作用独特。总结澳门“一国两制”成功实践，可以获得以下4点重要经验",
+        "url": "https://element-plus.org",
+      }
+    ]
+    return {Logo, answer, pages}
   }
 }
 </script>
@@ -24,12 +36,16 @@ export default {
       </el-row>
     </el-header>
     <el-main style="margin-top:50px; margin-left: 20px">
-      <el-row style="margin-top: 15px">
-        <el-text size="large">Large</el-text>
-      </el-row>
-      <el-row style="margin-top:20px">
-        <el-text size="default">Large</el-text>
-      </el-row>
+      <div v-for="page in pages">
+        <el-row style="margin-top: 15px">
+          <el-link href="{{ page.url }}" target="_blank">
+            <el-text size="large">{{ page.title }}</el-text>
+          </el-link>
+        </el-row>
+        <el-row style="margin-top:20px">
+          <el-text size="default">{{ page.describe }}</el-text>
+        </el-row>
+      </div>
     </el-main>
     <el-footer>
     </el-footer>
